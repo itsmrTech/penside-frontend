7f003b43f324c1c7dc50d70f2f27a7864de323fa
mrTech <shamot.group@gmail.com>
2017-11-23 15:57:28 +0330

    axios test

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,7 +1,24 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class Home extends Component {
-
+	testAxios=()=>{ 
+		console.log('pressed');
+		axios.post('http://192.168.21.102:3000/users/register', {
+			firstName: 'Fred',
+			lastName: 'Flintstone',
+			signature:'Kuhyari',
+			email:'Kuh@yar.me',
+			password:'123',
+			phone:"0912"
+		  })
+		  .then(function (response) {
+			console.log(response);
+		  })
+		  .catch(function (error) {
+			console.log(error);
+		  });
+	}
 	render() {
 		return (
 			<div>
@@ -9,6 +26,7 @@ export default class Home extends Component {
 				<div className="mainbar">
 					{this.props.children}
 				</div>
+				<input type="button" onClick={()=>this.testAxios()} value="Click me" />
 			</div>
 		)
 	}
